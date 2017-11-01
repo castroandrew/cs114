a402daa973dc3907bd90fb6ea47db7c5e0b9551c
s203 <s203@ai241lab0203m05.aipdcls.edu>
2017-11-01 08:43:30 -0700

    magic

---
+++ magic8ball.py
@@ -0,0 +1,37 @@
+print('Ask me about your future')
+input()
+import random
+
+print(random.randint(1,15))
+fortune=random.randint(1,15)
+
+if fortune==1:
+    print('It is certain')
+elif fortune==2:
+    print('it is decidedly so')
+elif fortune==3:
+    print('without a doubt')
+elif fortune==4:
+    print('yes definitely')
+elif fortune== 5:
+    print('You may rely on it')
+elif fortune==6:
+    print(' Do not count on it')
+elif fortune==7:
+    print(' My reply is no')
+elif fortune== 8:
+    print('My sources say no')
+elif fortune==9:
+    print(' Outlook not so good')
+elif fortune== 10:
+    print('Very doubtful')
+elif fortune== 11:
+    print('Reply hazy try again')
+elif fortune==  12:
+    print('Ask again later')
+elif fortune==13:
+    print('Better not tell you now')
+elif fortune== 14:
+    print('Cannot predict now')
+elif fortune== 15:
+    print('Concentrate and ask again')
