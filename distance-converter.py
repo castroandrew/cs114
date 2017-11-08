148176647421accc260f75ff35c0259792c2451f
s203 <s203@ai241lab0203m05.aipdcls.edu>
2017-11-08 10:13:48 -0800

    distance

---
+++ distance-converter.py
@@ -0,0 +1,103 @@
+print('===Distance Converter===')
+print('enter the number you wish to do')
+print('#1 miles -> kilometers')
+print('#2 miles -> feet')
+print('#3 miles -> meter')
+print('#4 miles -> inch')
+print('#5 miles -> centimeters')
+print('#6 kilometers -> miles')
+print('#7 kilometers -> feet')
+print('#8 kilometers -> meter')
+print('#9 kilometers -> inch')
+print('#10 kilometers -> centimeters')
+print('#11 feet -> miles ')
+print('#12 feet -> kilometers')
+print('#13 feet -> meter')
+print('#14 feet -> inch')
+print('#15 feet -> centimeters')
+print('#16 meter -> miles')
+print('#17 meter -> kilometers')
+print('#18 meter -> feet')
+print('#19 meter -> inch')
+print('#20 meter -> centimeters')
+print('#21 feet -> miles ')
+print('#22 feet -> kilometers')
+print('#23 feet -> meter')
+print('#24 feet -> inch')
+print('#25 feet -> centimeters')
+print('#26 inch -> miles')
+print('#27 inch -> kilometers')
+print('#28 inch -> meter')
+print('#29 inch -> feet')
+print('#30 inch -> centimeters')
+print('#31 centimeters -> miles ')
+print('#32 centimeters -> kilometers')
+print('#33 centimeters -> meter')
+print('#34 centimeters -> inch')
+print('#35 centimeters -> feet')
+
+input()=converter
+print('enter amount')
+input()= float(amount)
+if converter==1:
+    print(float(amount)* )
+elif converter==2:
+        print(float(amount)* )
+elif converter==3:
+        print(float(amount)* )
+elif converter==4:
+        print(float(amount)* )
+elif converter==5:
+        print(float(amount)* )
+elif converter==6:
+        print(float(amount)* )
+elif converter==7:
+        print(float(amount)* )
+elif converter==8:
+        print(float(amount)* )
+elif converter==9:
+        print(float(amount)* )
+elif converter==10:
+        print(float(amount)* )
+elif converter==11:
+        print(float(amount)* )
+elif converter==12:
+        print(float(amount)* )
+elif converter==13:
+        print(float(amount)* )
+elif converter==14:
+        print(float(amount)* )
+elif converter==15:
+        print(float(amount)* )
+elif converter==16:
+        print(float(amount)* )
+elif converter==17:
+        print(float(amount)* )
+elif converter==18:
+        print(float(amount)* )
+elif converter==19:
+        print(float(amount)* )
+elif converter==20:
+        print(float(amount)* )
+elif converter==21:
+        print(float(amount)* )
+elif converter==22:
+        print(float(amount)* )
+elif converter==23:
+        print(float(amount)* )
+elif converter==24:
+        print(float(amount)* )
+elif converter==25:
+        print(float(amount)* )
+elif converter==26:
+        print(float(amount)* )
+elif converter==27:
+        print(float(amount)* )
+elif converter==28:
+        print(float(amount)* )
+elif converter==29:
+        print(float(amount)* )
+elif converter==30:
+        print(float(amount)* )
+else
+print('the convertion you selected is invaled')
