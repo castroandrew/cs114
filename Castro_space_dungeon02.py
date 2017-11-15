fe5282ec3b88bc6a76b3435999cb10e59d79ae6e
s203 <s203@2024-01-203-05.aipdcls.edu>
2017-11-15 09:59:41 -0800

    update

--- Castro_space_dungeon02.py
+++ Castro_space_dungeon02.py
@@ -32,114 +32,235 @@ print("What is your name")
 print('')
 name = input()
 print('')
-print("welcome " +name+ "You have "+magic+" magic points and "+attack+ " attack points")
+print("Welcome " +name+ ", You have "+magic+" magic points and "+attack+ " attack points")
 print("and is a "+classes+" class.")
 print('')
 print('')
 health=100
 enemies_health=80
-while health== 0:
-    print('you are dead')
-    # this is the health block
-    sys.exit(0)
 
-while magic== 0:
-    print('')
-    print(' you ran out of mp and past out.')
-    print('now your life is up to faith.')
-    if health== 0:
-        int(magic) +1
-    else:
-        print('you wake up with scars all over and bleeding. You retreat to live another day')
-    sys.exit(0)
+# while health== 0:
+    # print('you are dead')
+    # sys.exit(0)
+# this is the health block
+
+
+# while magic== 0:
+    # print('')
+    # print(' you ran out of mp and past out.')
+    # print('now your life is up to faith.')
+    # if health== 0:
+    #     int(magic) +1
+    # else:
+    #     print('you wake up with scars all over and bleeding. You retreat to live another day')
+    # sys.exit(0)
     # what happens when you run out of mp
-move=false
 
-while move==stats:
+move='false'
+
+# while move=='stats':
     print('')
     print('you have '+health+' hp left')
     print('and '+magic+' mp left')
-    move=false
+    move='false'
     input()
-    fight=fight
+    fight='fight'
     print('')
-    # this is the stats block
-run= stay
-while run== run:
-    print('You ran away like a wuss.')
-    sys.exit(0)
-    # Run away block
+# this is the stats block
 
-fight= false
+run= 'stay'
 
-while fight==fight:
-    if health==0:
-        print('')
-        print('Nooooooo')
-    elif magic==0:
-        print('')
-        print('You feel sleepy')
-    elif enemies_health==0:
-        print('')
-        print('you did it. Now what were you in here for.')
-        print('')
-        print('')
-        print('all well, it must have not been that important.')
-        print('You left the room as a victor')
-        sys.exit(0)
-    # Fight check
+# while run== 'run':
+#     print('You ran away like a wuss.')
+#     sys.exit(0)
+# Run away block
 
+fight='false'
+
+# while fight=='fight':
+#     if health==0:
+#         print('')
+#         print('Nooooooo')
+#     elif magic==0:
+#         print('')
+#         print('You feel sleepy')
+#     elif enemies_health==0:
+#         print('')
+#         print('you did it. Now what were you in here for.')
+#         print('')
+#         print('')
+#         print('all well, it must have not been that important.')
+#         print('You left the room as a victor')
+#         sys.exit(0)
+# Fight check
 
 
 
 print('')
 print('you have enter a dull gray room in your ship.')
-
-if classes==str(Space Duck):
+print('')
+if classes== "Space Duck":
     print('It was then your mortal enemies attack ')
     print('The Ships CAT')
-elif classes== That One Human:
+elif classes== "That One Human":
     print('It was then your mortal enemies attack ')
     print('That Guy')
-elif classes== Sexy Alien:
+elif classes== "Sexy Alien":
     print('It was then your mortal enemies attack ')
     print('reality')
-
+# veryed text story
 print('the fight begins')
 
 print('what are you going to do fight or run')
 fight= input()
 
-while fight==fight:
-    print(' your move. attack, magic, stats')
-    move= input()
-    if move== attack:
-        print('you have attacked')
-        attack_random= int(random.randint(1,5) + attack)
-        print('you did'+attack_random+' damege.')
-        enemies_health= int(enemies_health - attack_random + attack)
-        move= over
-    elif move== magic
-        print('you have used magic')
-        magic_random= int(random.randint(1,5)+ 20)
-        print('you did'+magic_random+' damege.')
-        enemies_health= int(enemies_health - magic_random)
-        move= over
-    elif move = stats
-        print('')
-        fight=pause
-    if move== over:
-        print('Now they attack')
-        fight= thier turn
-# player fight phases block
+while run== 'run':
+    print('You ran away like a wuss.')
+    sys.exit(0)
+# Run away block
 
-while fight== thier turn:
+Battle= 'start'
+
+while Battle=='start:':
+    while fight=='fight':
+        if health==0:
+            print('')
+            print('Nooooooo')
+        elif magic==0:
+            print('')
+            print('You feel sleepy')
+        elif enemies_health==0:
+            print('')
+            print('you did it. Now what were you in here for.')
+            print('')
+            print('')
+            print('all well, it must have not been that important.')
+            print('You left the room as a victor')
+            sys.exit(0)
+        # Fight check
+    # Fight check
+    while fight=='fight':
+        print(' your move. attack, magic, stats')
+        move= input()
+        if move== 'attack':
+            print('you have attacked')
+            attack_random= random.randint(1,5) + int(attack)
+            print('you did'+ str(attack_random) +' damege.')
+            enemies_health= int(enemies_health) - int(attack_random)
+            move= 'over'
+        elif move== 'magic':
+            print('you have used magic')
+            magic_random= random.randint(1,5)+ 20
+            print('you did '+ str(magic_random) +' damege.')
+            enemies_health= int(enemies_health) - int(magic_random)
+            move= 'over'
+        elif move == 'stats':
+            print('')
+            fight='pause'
+        if move== 'over':
+            print('Now they attack')
+            fight= 'thier turn'
+    # player fight phases block
+
+    while move=='stats':
+        print('')
+        print('you have '+health+' hp left')
+        print('and '+magic+' mp left')
+        move='false'
+        input()
+        fight='fight'
+        print('')
+    # this is the stats block
+
+    while fight=='fight':
+        if health==0:
+            print('')
+            print('Nooooooo')
+        elif magic==0:
+            print('')
+            print('You feel sleepy')
+        elif enemies_health==0:
+            print('')
+            print('you did it. Now what were you in here for.')
+            print('')
+            print('')
+            print('all well, it must have not been that important.')
+            print('You left the room as a victor')
+            sys.exit(0)
+    # Fight check
+
+    while fight=='fight':
+        print(' your move. attack, magic, stats')
+        move= input()
+        if move== 'attack':
+            print('you have attacked')
+            attack_random= random.randint(1,5) + int(attack)
+            print('you did'+ str(attack_random) +' damege.')
+            enemies_health= int(enemies_health) - int(attack_random)
+            move= 'over'
+        elif move== 'magic':
+            print('you have used magic')
+            magic_random= random.randint(1,5)+ 20
+            print('you did '+ str(magic_random) +' damege.')
+            enemies_health= int(enemies_health) - int(magic_random)
+            move= 'over'
+        elif move == 'stats':
+            print('')
+            fight='pause'
+        if move== 'over':
+            print('Now they attack')
+            fight= 'thier turn'
+    # player fight phases block
+
+    while enemies_health== 0:
+        print(' They are dead')
+        Battle='over'
+    # this is the enemies_health block
+
+    while magic== 0:
+        print('')
+        print(' you ran out of mp and past out.')
+        print('now your life is up to faith.')
+        if health== 0:
+            int(magic) +1
+        else:
+            print('you wake up with scars all over and bleeding. You retreat to live another day')
+            print('Game Over')
+            sys.exit(0)
+    # what happens when you run out of mp
+
+    while fight== 'thier turn':
     health_hit= int(health - random.randint(25,40))
     health= int(health - health_hit)
-    print('you took'+health_hit+' damege')
-    if health <= 0
+    print('you took '+ str(health_hit) +' damege')
+    if health <= 0:
         health = 0
-        fight= game over
-    else
-        fight=fight
-# enemies fight phases block
+        fight= 'game over'
+    elif health >=0:
+        fight='fight'
+    # enemies fight phases block
+
+    while fight=='fight':
+        if health==0:
+            print('')
+            print('Nooooooo')
+        elif magic==0:
+            print('')
+            print('You feel sleepy')
+        elif enemies_health==0:
+            print('')
+            print('you did it. Now what were you in here for.')
+            print('')
+            print('')
+            print('all well, it must have not been that important.')
+            print('You left the room as a victor')
+            sys.exit(0)
+    # Fight check
+
+    while health== 0:
+        print('you are dead')
+        sys.exit(0)
+    # this is the health block
+# This is the Block for a battle
+print('')
+print('You did it')
