b22b2e8bbd345c4ddfa46d9502bcd8c721dd7052
s203 <s203@2024-01-203-05.aipdcls.edu>
2017-11-22 10:27:42 -0800

    update

--- practice:madlib.py
+++ practice:madlib.py
@@ -1,4 +1,4 @@
-print('&&& Madlibs &&&')"""come back to this after i learn functions"""
+print('&&& Madlibs &&&')
 print('==================================')
 print('There once was a _____ From _____. ')
 print(' Who dreamed that it was ____ a _____.')
@@ -6,82 +6,116 @@ print('it woke with a ____ ,in the middle of the night,')
 print(' to see that its dream had come true.')
 print('===================================')
 print('please give a noun for line one')
-line_one= str(input())
+line_one= input()
 print('Would you like to see so far?')
-input()
-    # if: input()= yes
-    #     print('==================================')
-    #     print('There once was a '+ line_one+'From _____. ')
-    #     print(' Who dreamed that it was ____ a _____.')
-    #     print('it woke with a ____ ,in the middle of the night,')
-    #     print(' to see that its dream had come true.')
-    #     print('===================================')
-    #     print('would you like to change line one?')
-    #     input()
+view=input()
+
+while view== yes:
+    print('==================================')
+    print('There once was a '+ str(line_one)+'From _____. ')
+    print(' Who dreamed that it was ____ a _____.')
+    print('it woke with a ____ ,in the middle of the night,')
+    print(' to see that its dream had come true.')
+    print('===================================')
+    print('would you like to change line one?')
+    change_line_one=input()
+    if change_line_one==yes:
+        print('please give a noun for line one')
+        line_one= input()
+    else:
+        view=no
+
+
+
 
 print('please give a place for line two')
-line_two= str(input())
+line_two= input()
 print('Would you like to see so far?')
-input()
-    # if: input()= yes
-    #     print('==================================')
-    #     print('There once was a '+ line_one+'From ' line_two)
-    #     print(' Who dreamed that it was ____ a _____.')
-    #     print('it woke with a ____ ,in the middle of the night,')
-    #     print(' to see that its dream had come true.')
-    #     print('===================================')
-    #     print('would you like to change line one?')
-    #     input()
+view=input()
 
-    print('please give a adjective for line three')
-    line_three= input()
-    print('Would you like to see so far?')
-    input()
+while view== yes:
+    print('==================================')
+    print('There once was a '+ str(line_one)+'From ' +str(line_two))
+    print(' Who dreamed that it was ____ a _____.')
+    print('it woke with a ____ ,in the middle of the night,')
+    print(' to see that its dream had come true.')
+    print('===================================')
+    print('would you like to change line two?')
+    change_line_two=input()
+    if change_line_one==yes:
+        print('please give a noun for line two')
+        line_two= input()
+    else:
+        view=no
 
-    # if: input()= yes
-    #     print('==================================')
-    #     print('There once was a '+ line_one+'From '+ line_two)
-    #     print(' Who dreamed that it was'+line_three+ 'a _____.')
-    #     print('it woke with a ____ ,in the middle of the night,')
-    #     print(' to see that its dream had come true.')
-    #     print('===================================')
-    #     print('would you like to change line one?')
-    #     input()
+
+
+
+
+print('please give a adjective for line three')
+line_three= input()
+print('Would you like to see so far?')
+view=input()
+
+while view== yes:
+    print('==================================')
+    print('There once was a '+ str(line_one)+'From ' +str(line_two))
+    print(' Who dreamed that it was '+str(line_three)+ ' a _____.')
+    print('it woke with a ____ ,in the middle of the night,')
+    print(' to see that its dream had come true.')
+    print('===================================')
+    print('would you like to change line three?')
+    change_line_three=input()
+    if change_line_three==yes:
+        print('please give a noun for line three')
+        line_three= input()
+    else:
+        view=no
 
 print('please give a noun for line four')
-line_four= str(input())
+line_four= input()
 print('Would you like to see so far?')
-input()
+view=input()
 
-    # if: input()= yes
-    #     print('==================================')
-    #     print('There once was a '+ line_one+'From '+ line_two)
-    #     print(' Who dreamed that it was'+line_three+ 'a '+line_four)
-    #     print('it woke with a ____ ,in the middle of the night,')
-    #     print(' to see that its dream had come true.')
-    #     print('===================================')
-    #     print('would you like to change line one?')
-    #     input()
+while view== yes:
+    print('==================================')
+    print('There once was a '+ str(line_one)+'From ' +str(line_two))
+    print(' Who dreamed that it was '+str(line_three)+ 'a '+str(line_four))
+    print('it woke with a ____ ,in the middle of the night,')
+    print(' to see that its dream had come true.')
+    print('===================================')
+    print('would you like to change line four?')
+    change_line_four=input()
+    if change_line_four==yes:
+        print('please give a noun for line four')
+        line_four= input()
+    else:
+        view=no
 
 print('please give a adjective for line five')
-line_five= str(input())
+line_five= input()
 print('Would you like to see so far?')
-input()
+view=input()
 
-    # if: input()= yes
-    #     print('==================================')
-    #     print('There once was a '+ line_one+'From '+ line_two)
-    #     print(' Who dreamed that it was'+line_three+ 'a '+line_four)
-    #     print('it woke with a'+line_five+ ',in the middle of the night,')
-    #     print(' to see that its dream had come true.')
-    #     print('===================================')
-    #     print('would you like to change line one?')
-    #     input()
+while view== yes:
+    print('==================================')
+    print('There once was a '+ str(line_one)+'From ' +str(line_two))
+    print(' Who dreamed that it was '+str(line_three)+ 'a '+str(line_four))
+    print('it woke with a'+str(line_five)+ ',in the middle of the night,')
+    print(' to see that its dream had come true.')
+    print('===================================')
+    print('would you like to change line five?')
+    change_line_five=input()
+    if change_line_five==yes:
+        print('please give a noun for line five')
+        line_five= input()
+    else:
+        view=no
 
 print('Here is your Madlibs')
 print('==================================')
-print('There once was a '+ line_one+'From '+ line_two)
-print(' Who dreamed that it was'+line_three+ 'a '+line_four)
-print('it woke with a'+line_five+ ',in the middle of the night,')
+print('There once was a '+ str(line_one)+'From ' +str(line_two))
+print(' Who dreamed that it was '+str(line_three)+ 'a '+str(line_four))
+print('it woke with a'+str(line_five)+ ',in the middle of the night,')
 print(' to see that its dream had come true.')
 print('===================================')
---
+++ space_dungon_random_enemy.py
@@ -0,0 +1,17 @@
+import random
+people=['Cat','Blob','Ule','AI','Jaba']
+
+Print('Ladies and gentelman of the world')
+Print('I would like to introduce to you...')
+enemy= random.randint(1,5)
+
+if enemy==1:
+    print(people[0])
+elif enemy==2:
+    print(people[1])
+elif enemy==3:
+    print(people[2])
+elif enemy==4:
+    print(people[3])
+elif enemy==5:
+    print(people[4])
